f56e67874c362f2f8050e35e43b36233cf3c357e
ingundela <ingundela@users.noreply.github.com>
2017-11-12 13:24:23 +0200

    webmaster

---
+++ js/main.js
@@ -0,0 +1,94 @@
+ // Select all links with hashes
+ $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+              // On-page links
+              if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+                && 
+                location.hostname == this.hostname
+              ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                  // Only prevent default if animation is actually gonna happen
+                  event.preventDefault();
+                  $('html, body').animate({
+                    scrollTop: target.offset().top
+                  }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                      return false;
+                    } else {
+                      $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                      $target.focus(); // Set focus again
+                    };
+                  });
+                }
+              }
+            });
+          
+//full slider height
+
+    $(function(){
+      'use strict';
+      var winH = $(window).height(),
+        upperH = $('.upper-bar').innerHeight(),
+        navH   = $('.navbar').innerHeight();
+        $('.slider, .carousel-item').height(winH - (upperH + navH));
+
+         //Features work shuffle
+        $('.feature-work ul li').on('click', function (){
+          $(this).addClass('active').siblings().removeClass('active');
+          if ($(this).data('class') === 'all'){
+            $('.shuffle-imgs .col-md').css('opacity', '1');
+          }
+          else{
+            $('.shuffle-imgs .col-md').css('opacity', '.08');
+            $($(this).data('class')).parent().css('opacity', 1);
+          }
+        });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
